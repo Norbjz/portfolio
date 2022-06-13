c60e1eb6a83f1e0a28028208a0c96d47ad9c04ea
Norberto Jiménez <norberto.j90@gmail.com>
2022-06-13 09:05:18 -0500

    Dark theme, animations and media queries

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var last_known_scroll = 0;
+var themeToggle = true;
+
 function moveBackground(e) {
   const shapes = document.querySelectorAll('.shape');
   const x = e.clientX * 0.04;
@@ -7,5 +10,25 @@ function moveBackground(e) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
     shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
+/* function show() {
+  document.getElementById('contact').style.display = 'block';
+
+  let cosa = document.getElementById('contact');
+  console.log(cosa);
+}
+
+function hide() {
+  document.getElementById('contact').style.display = 'none';
+} */
+
+function toggleTheme() {
+  themeToggle = !themeToggle;
+  if (themeToggle) {
+    document.body.classList.add('white-theme');
+  } else {
+    document.body.classList.remove('white-theme');
   }
 }
